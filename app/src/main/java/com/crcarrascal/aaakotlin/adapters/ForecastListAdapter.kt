98072ed47afee38bc0f86de3da3be7d5ffc4e855
Carl Stephen Carrascal <carl.carrascal@igentechnologies.com>
2018-07-11 19:58:45 +0800

    formatted forecasts

--- app/src/main/java/com/crcarrascal/aaakotlin/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/crcarrascal/aaakotlin/adapters/ForecastListAdapter.kt
@@ -6,6 +6,9 @@ import android.widget.TextView
 import com.crcarrascal.aaakotlin.webservices.models.Forecast
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import java.text.DateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by crcarrascal on 10/07/2018.
@@ -22,13 +25,16 @@ class  ForecastListAdapter(val items : List<Forecast>) : RecyclerView.Adapter<Fo
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
         info { "$items[position].weather[0].description" }
-        with(items[position].weather[0]){
-            holder!!.textView.text = "$description"
+        with(items[position]){
+            val dt = Calendar.getInstance().timeInMillis + TimeUnit.DAYS.toMillis(position.toLong())
+            val df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
+
+            holder!!.textView.text = "${df.format(dt)} ${weather[0].description} ${temp.max}C ${temp.min}C"
         }
     }
 
     fun addUpdateData(items : List<Forecast>){
-        
+
         notifyDataSetChanged()
     }
 
